7f5ee5bdc82b90e5ecfb4bbdbc1357156b553ee1
Rafael <rafael.alves.proff@gmail.com>
2018-04-09 01:28:03 +0100

    O meu 1º Web Scraper em Python

---
+++ 1.py
@@ -0,0 +1,17 @@
+a="asdsa"
+print (a)
+
+i=10
+p=10
+l=[1,2,3,4]
+
+def ola(a,b,c):
+	print(a,b,c)
+
+for i in l:
+	if i%2:
+		print(i)
+	elif i==2:
+		print("cenas1")
+	else:
+		ola("ol","ola","juk")
